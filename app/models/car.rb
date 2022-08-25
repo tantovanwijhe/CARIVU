f9620e5d57ad09b3eb8d0f11321e646f11b4eb49
nazanin bos <nazanin.manfaz@gmail.com>
2022-08-25 14:34:19 +0200

    Refactor code

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,8 +1,13 @@
 class Car < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  CATEGORIES = ["Trucks", "Vans", "Exotic & Luxury", "Convertibles", "Sport", "Classics", "Minivans", "SUVs", "Electric vehicles"]
   has_many_attached :photos
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+
+  CATEGORIES = ["Trucks", "Vans", "Exotic & Luxury", "Convertibles", "Sport", "Classics", "Minivans", "SUVs", "Electric vehicles"]
+
   validates :brand_model, presence: true
   validates :price, presence: true
   validates :category, inclusion: { in: CATEGORIES }

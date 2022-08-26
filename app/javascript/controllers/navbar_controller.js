4a13516dcc2fcd777627b178bf413a4c4462dd79
remi8931@gmail.com <remi8931@gmail.com>
2022-08-26 15:03:30 +0200

    landing page and navbar finished

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,11 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
-  updateNavbar() {
-    if (window.scrollY >= window.innerHeight) {
-      this.element.classList.add("navbar-lewagon-black")
-    } else {
-      this.element.classList.remove("navbar-lewagon-black")
+  static targets = ['navbar', 'banner']
+
+  connect() {
+    if (this.hasBannerTarget) {
+      this.navbarTarget.classList.remove("navbar-lewagon-black")
+      this.navbarTarget.classList.add("fixed-top")
     }
+  }
+
+  updateNavbar() {
+    if (this.hasBannerTarget) {
+      if (window.scrollY >= window.innerHeight) {
+        this.navbarTarget.classList.add("navbar-lewagon-black")
+      } else {
+        this.navbarTarget.classList.remove("navbar-lewagon-black")
+      }
+    }
+
   }
 }
